792d9cc0a52aa485c715cec0699408b78252d0c9
Lamego <m.lamego.soriano@outlook.com>
2023-03-05 23:03:21 -0600

    Página de resultados

--- Tarea 4/src/rutas/index.js
+++ Tarea 4/src/rutas/index.js
@@ -3,12 +3,18 @@ const path = require('path');
 // Requerir router de express
 const router = require('express').Router();
 
-const rutasResultados = require('./resultados');
+//const rutasResultados = require('./resultados');
 
 // El primer parámetro que recibe es el parámetro de petición y el segundo es el de respuesta
-function cargarResultados(req,res) {
+function cargarBuscador(req,res) {
     console.log('El API funciona');
     res.render('buscador');
+
+}
+
+function verNoticia (req,res) {
+    const keyWord = req.params.keyWord;
+    res.render('resultados', {keyWord : 'rip'})
 }
 
 // El middleware se usará en todas, así que se pone aquí v:
@@ -16,8 +22,8 @@ const auth = require('../middlewares').auth;
 
 router.use('', auth);
 
-router.get('/', auth, cargarResultados);
+router.get('/', auth, cargarBuscador);
 
-router.use('/resultados', rutasResultados);
+router.get('/resultados', verNoticia);
 
 module.exports = router;
--- Tarea 4/src/rutas/resultados.js
+++ Tarea 4/src/rutas/resultados.js
@@ -2,4 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Requerir controlador
+//const controlador = require('./../controladores/resultados')
+
+// Listado específico
+//router.get('/', controlador.verNoticia);
+
 module.exports = router; // Es un middleware
