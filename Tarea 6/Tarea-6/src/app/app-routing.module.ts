ea6ec0dba3bf967807674905aa1bc2a01a91bd2e
Lamego <m.lamego.soriano@outlook.com>
2023-04-01 08:40:33 -0600

    Info en cada página

--- Tarea 6/Tarea-6/src/app/app-routing.module.ts
+++ Tarea 6/Tarea-6/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Rutas para cada componente
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { EducacionComponent } from './pages/educacion/educacion.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ExtracurricularComponent } from './pages/extracurricular/extracurricular.component';
+import { InfoContactoComponent } from './pages/info-contacto/info-contacto.component';
+
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'education', component: EducacionComponent},
+  {path: 'projects', component: ProyectosComponent},
+  {path: 'extracurricular', component: ExtracurricularComponent},
+  {path: 'contact', component: InfoContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
